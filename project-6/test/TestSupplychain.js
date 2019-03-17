7ae92d2a6221e52d97fadf16b10279fae8f86c42
Adrian Mohnacs <amohnacss@gmail.com>
2019-03-17 11:29:07 -0700

    init and structure

--- project-6/test/TestSupplychain.js
+++ project-6/test/TestSupplychain.js
@@ -14,7 +14,7 @@ contract('SupplyChain', function(accounts) {
     const originFarmLongitude = "144.341490"
     var productID = sku + upc
     const productNotes = "Best beans for Espresso"
-    const productPrice = web3.toWei(1, "ether")
+    const productPrice = web3.utils.toWei('1', "ether")
     var itemState = 0
     const distributorID = accounts[2]
     const retailerID = accounts[3]
@@ -50,7 +50,7 @@ contract('SupplyChain', function(accounts) {
         
         // Watch the emitted event Harvested()
         var event = supplyChain.Harvested()
-        await event.watch((err, res) => {
+        await event.on('data', event => {
             eventEmitted = true
         })
 
